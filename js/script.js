e4d1a6ffd413af5ceaa95f0dad1d35bf8fa5bf26
Afu Sidhi Pamekas <afusidhipamekas@gmail.com>
2020-02-13 22:01:15 +0700

    0.3 (responsive + footer)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
+const nav = document.querySelector('.navbar');
+const nav_links = document.querySelector('.navbar_nav-links');
+const burger = document.querySelector('.navbar_burger');
+const dot = document.querySelector('.navbar_bar_dot');
+
+// Scroll method
 const scrollPage = () => {
-    const nav = document.querySelector('.navbar');
     window.onscroll = function () {
         scrollFunction();
     };
@@ -7,13 +12,31 @@ const scrollPage = () => {
         if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
         } else if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
             nav.classList.add('navbar_scroll');
+            nav_links.classList.add('navbar_nav-links_scroll');
         } else {
             nav.classList.remove('navbar_scroll');
+            nav_links.classList.remove('navbar_nav-links_scroll');
         }
     }
+};
+
+// Burger method
+const burgerClick = () => {
+    burger.addEventListener('click', () => {
+        nav_links.classList.toggle('navbar_nav-links_click');
+        burger.classList.toggle('navbar_burger_click');
+    });
+};
+const dotClick = () => {
+    dot.addEventListener('click', () => {
+        nav_links.classList.toggle('navbar_nav-links_click');
+        dot.classList.toggle('navbar_bar_dot_click');
+    });
 };
 
 AOS.init({
     duration: 1200
 });
 scrollPage();
+burgerClick();
+dotClick();
